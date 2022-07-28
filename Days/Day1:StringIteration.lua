63d657224d298702346812610b2ff38d1009eae1
Muheko Nikolas <13nikolusmuheko.gmail.com>
2022-07-28 10:29:41 +0000

    Updated Day 1

--- Days/Day1:StringIteration.lua
+++ Days/Day1:StringIteration.lua
@@ -12,7 +12,7 @@ stringLength = string.len(stri)
 --For loops in strings
 for c=1, stringLength do --Am taking this approach because it gives me access to the index of the character
     g = string.sub(stri, c, c) --my first idea here was to use bracket indexing but that only returned null. so i had to take a this aproach
-    --print(g)
+    print(g)
 end
 
 
